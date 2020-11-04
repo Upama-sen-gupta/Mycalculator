6263ffe91c4d6c4dba1c8551f7e6bd237e377320
upama-sen-gupta <upamasen98@gmail.com>
2020-11-04 21:28:35 +0600

    project 1(calculator)

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //DEclare object as global
+    TextView txtResult;
+    EditText etFirstNumber,etSecondNumber;
+    Button btnPlus,btnMinus,btnMulti,btnDivision,btnClear;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //button initialize start
+        txtResult=findViewById(R.id.txt_result);
+        etFirstNumber=findViewById(R.id.et_first_number);
+        etSecondNumber=findViewById(R.id.et_second_number);
+
+        btnPlus=findViewById(R.id.btn_plus);
+        btnMinus=findViewById(R.id.btn_minus);
+        btnMulti=findViewById(R.id.btn_multi);
+        btnDivision=findViewById(R.id.btn_division);
+        btnClear=findViewById(R.id.btn_clear);
+
+btnPlus.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        String number1=etFirstNumber.getText().toString();
+        String number2=etSecondNumber.getText().toString();
+
+        if(number1.isEmpty()&& number2.isEmpty())
+        {
+            Toast.makeText(MainActivity.this, "please enter digit", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            double value1=Double.parseDouble(number1);
+            double value2=Double.parseDouble(number2);
+            double result=value1+value2;
+            txtResult.setText(""+result);
+
+        }
+
+    }
+});
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number1=etFirstNumber.getText().toString();
+                String number2=etSecondNumber.getText().toString();
+
+                if(number1.isEmpty()&& number2.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "please enter digit", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    double value1=Double.parseDouble(number1);
+                    double value2=Double.parseDouble(number2);
+                    double result=value1-value2;
+                    txtResult.setText(""+result);
+
+                }
+
+            }
+        });
+
+
+        btnMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number1=etFirstNumber.getText().toString();
+                String number2=etSecondNumber.getText().toString();
+
+                if(number1.isEmpty()&& number2.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "please enter digit", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    double value1=Double.parseDouble(number1);
+                    double value2=Double.parseDouble(number2);
+                    double result=value1*value2;
+                    txtResult.setText(""+result);
+
+                }
+
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number1=etFirstNumber.getText().toString();
+                String number2=etSecondNumber.getText().toString();
+
+                if(number1.isEmpty()&& number2.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "please enter digit", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    double value1=Double.parseDouble(number1);
+                    double value2=Double.parseDouble(number2);
+                    double result=value1/value2;
+                    txtResult.setText(""+result);
+
+                }
+
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtResult.setText("Result");
+                etFirstNumber.setText("");
+                etSecondNumber.setText("");
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+}
